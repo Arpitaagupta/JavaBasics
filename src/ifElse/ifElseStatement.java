116a3a47349c221b81ede4d574aa1174919c35bd
Arpita Gupta <arpitagupta6620@gmail.com>
2022-12-20 20:44:06 +0530

    Java Basics

---
+++ src/ifElse/ifElseStatement.java
@@ -0,0 +1,26 @@
+package ifElse;
+import java.util.Scanner;
+
+public class ifElseStatement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter your age");
+        int age = sc.nextInt();
+        if(age>18) {
+            System.out.println("you can drive");
+        } else
+            System.out.println("you cannot drive");
+
+        // this is another method of writing if else statements
+
+       /* int age = 24;
+        boolean cond = (age==18);
+        if(cond){
+            System.out.println("you can drive");
+       } else
+            System.out.println("you cannot drive");
+
+        */
+    }
+}
